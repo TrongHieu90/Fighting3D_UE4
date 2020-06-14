70910027fd4597575781a8f729f2de0af69e9199
Hieu Nguyen <tronghieu.designer@gmail.com>
2020-06-14 17:22:24 +0700

    + Adding character-based player script

---
+++ Source/Fighting3D/PlayerCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ArenaCharacter.h"
+#include "PlayerCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FIGHTING3D_API APlayerCharacter : public AArenaCharacter
+{
+	GENERATED_BODY()
+	
+};
