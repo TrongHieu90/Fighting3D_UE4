b59eee0f2537d0c95ab5ff14a12143ab0d2a01a8
Trong Hieu <tronghieu.designer@gmail.com>
2020-07-09 23:16:52 +0700

    + Added class for pickable weapons

---
+++ Source/Fighting3D/PickableWeapon.cpp
@@ -0,0 +1,7 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PickableWeapon.h"
+#include "Components/SphereComponent.h"
+#include "Components/SkeletalMeshComponent.h"
+
---
+++ Source/Fighting3D/PickableWeapon.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PickableBase.h"
+#include "PickableWeapon.generated.h"
+
+class UBoxComponent;
+class USphereComponent;
+class USkeletalMeshComponent;
+
+UCLASS()
+class FIGHTING3D_API APickableWeapon : public APickableBase
+{
+	GENERATED_BODY()
+public:
+    /*APickableWeapon();*/
+	UBoxComponent* GetDamageBox() const;
+
+protected:
+	UPROPERTY(EditAnywhere)
+		UBoxComponent* damageBox;
+
+	UPROPERTY(EditAnywhere)
+		USphereComponent* mySphere;
+
+	UPROPERTY(EditAnywhere)
+		USkeletalMeshComponent* skeletalMesh;
+};
