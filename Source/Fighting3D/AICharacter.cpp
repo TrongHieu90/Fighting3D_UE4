37acb184703b0fd28e716ba9ae87482af4ce6f18
Trong Hieu <tronghieu.designer@gmail.com>
2020-07-19 23:10:02 +0700

    + Add Ai script

---
+++ Source/Fighting3D/AICharacter.cpp
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AICharacter.h"
+#include "PickableBase.h"
+#include "PickableWeapon.h"
+#include "Engine/World.h"
+#include "AIController.h"
+#include "BrainComponent.h"
+
+void AAICharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	CreateAndAttachWeapon();
+}
+
+void AAICharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AAICharacter::CreateAndAttachWeapon()
+{
+	FVector location = GetActorLocation();
+	FRotator rotation = GetActorRotation();
+
+	if (Shield != nullptr)
+	{
+		APickableBase* newShield = Cast<APickableBase>(GetWorld()->SpawnActor(Shield, &location, &rotation));
+		newShield->AttachItemTo(GetMesh(), TEXT("DualWeaponPoint"));
+	}
+	if (Weapon != nullptr)
+	{
+		APickableWeapon* newWeapon = Cast<APickableWeapon>(GetWorld()->SpawnActor(Weapon, &location, &rotation));
+		newWeapon->AttachItemTo(GetMesh(), TEXT("WeaponPoint"));
+	}
+}
+
---
+++ Source/Fighting3D/AICharacter.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ArenaCharacter.h"
+#include "AICharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FIGHTING3D_API AAICharacter : public AArenaCharacter
+{
+	GENERATED_BODY()
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		TSubclassOf<APickableBase> Shield;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		TSubclassOf<APickableWeapon> Weapon;
+
+	void CreateAndAttachWeapon();
+};
